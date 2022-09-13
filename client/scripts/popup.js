9209ed15568db2dc1942b560b6829eaf860a1f09
Rohan Mathur <rmathur101@gmail.com>
2022-09-13 17:55:39 -0500

    add functions for getting info from dom; as well as function to handle submit to backend

--- client/scripts/popup.js
+++ client/scripts/popup.js
@@ -1,37 +1,81 @@
 // TODO: need to look at firefox implementation / web standard to see if I can generalize
 // TODO: and / or need to see if I can avoid callbacks (in favor of promises)
-// NOTE: this is functionality for getting the url, commenting out for now
-// chrome.tabs.query({
-//   active: true,
-//   lastFocusedWindow: true
-// }, function(tabs) {
-//   var tab = tabs[0];
-//   let bookmarkURLElem = document.getElementById("bookmark-url")
-//   bookmarkURLElem.href = tab.url
-//   bookmarkURLElem.textContent = tab.url 
-// });
+
+async function getActiveTabURL() {
+  let [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true});
+  // TODO: need to handle the case where we are not able to find the tab (active: true, etc.)
+  return tab.url 
+}
+
+function getIsBookmarkCheckedBool() {
+ let elem = document.getElementById("bookmark-checkbox-input") 
+ if (elem.checked == true) {
+  return true
+ } else {
+  return false
+ }
+}
+
+function getIsShareCheckedBool() {
+ let elem = document.getElementById("share-checkbox-input") 
+ if (elem.checked == true) {
+  return true
+ } else {
+  return false
+ }
+}
+
+function getShareInfo() {}
+
+function getTagInfo() {}
+
+let submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", async () => {
+  let tabURL = await getActiveTabURL()
+  let isBookmarkChecked = getIsBookmarkCheckedBool() 
+  let isShareChecked = getIsShareCheckedBool()
+  console.log("bookmarkURL")
+  console.log(tabURL)
+  console.log("shouldBookmark")
+  console.log(isBookmarkChecked)
+  console.log("shouldShare")
+  console.log(isShareChecked)
+  try {
+    const response = await axios.post(CONFIG.API_ENDPOINT + "submit_bookmark", {
+      bookmarkURL: tabURL,
+      shouldBookmark: isBookmarkChecked,
+      shouldShare: isShareChecked 
+    })
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
+// BELOW CODE IS CHROME EXTENSION TUTORIAL -------------------------------------------------------------------------
 
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+// let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// changeColor.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: setPageBackgroundColor,
-  });
-});
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     func: setPageBackgroundColor,
+//   });
+// });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     document.body.style.backgroundColor = color;
+//   });
+// }
