7320037c5946e1dec277e1aa7cb6890f8dcbc993
Rohan Mathur <rmathur101@gmail.com>
2022-09-13 17:52:23 -0500

    add boilerplate for template for config

---
+++ client/config/config_template.js
@@ -0,0 +1,4 @@
+// TODO: use this template to create your own config file called "config.js". This file should look exactly like this file, but it should have your own dev/prod values for the corresponding keys. For example, to run in dev, you should put your own local dev server url for "API_ENDPOINT". Once you create this new file, put it in the same folder as this file which is the "config" folder. Your own config file called "config.js" will be ignored by git. 
+const CONFIG = {
+  API_ENDPOINT: ""
+}
