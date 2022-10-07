68a52da6b90a22d23726171c64e7ea220a9b1c6c
Rohan Mathur <rmathur101@gmail.com>
2022-10-07 11:51:07 -0500

    add functionality for hiding / showing tab conts; add functionality for pull urls and displaying them in saved tab

--- client/scripts/popup.js
+++ client/scripts/popup.js
@@ -198,26 +198,61 @@ document.addEventListener("DOMContentLoaded", async(e) => {
   bookmarkTitleInputElem.value = title 
 });
 
+function toggleCreateBookmarkTab(on=true) {
+  let tabContElem = document.getElementById('create-bookmark-cont')
+  let tabElem = document.getElementById('create-bookmark-tab')
+  if (on) {
+    tabContElem.style.display = ""
+    tabElem.classList.add("selected-tab")
+  } else {
+    tabContElem.style.display = "none"
+    tabElem.classList.remove("selected-tab")
+  }
+}
+
+function toggleViewFeedTab(on=true) {
+  let tabContElem = document.getElementById('view-feed-cont')
+  let tabElem = document.getElementById('view-feed-tab')
+
+  if (on) {
+    tabContElem.style.display = ""
+    tabElem.classList.add("selected-tab")
+  } else {
+    tabContElem.style.display = "none"
+    tabElem.classList.remove("selected-tab")
+  }
+}
+
+function toggleViewBookmarksTab(on=true) {
+  let tabContElem = document.getElementById('view-bookmarks-cont')
+  let tabElem = document.getElementById('view-bookmarks-tab')
+
+  if (on) {
+    tabContElem.style.display = ""
+    tabElem.classList.add('selected-tab') 
+  } else {
+    tabContElem.style.display = "none"
+    tabElem.classList.remove('selected-tab') 
+  }
+}
+
+
 function showCreateBookmarkTab() {
-  let viewFeedContElem = document.getElementById('view-feed-cont')
-  viewFeedContElem.style.display = "none"
-
-  let createBookmarkContElem = document.getElementById('create-bookmark-cont')  
-  createBookmarkContElem.style.display = ""
-
-  document.getElementById('create-bookmark-tab').classList.add("selected-tab")
-  document.getElementById('view-feed-tab').classList.remove("selected-tab")
+  toggleViewBookmarksTab(false)
+  toggleViewFeedTab(false)
+  toggleCreateBookmarkTab(true)
 }
 
 function showViewFeedTab() {
-  let createBookmarkContElem = document.getElementById('create-bookmark-cont')  
-  createBookmarkContElem.style.display = "none"
+  toggleViewBookmarksTab(false)
+  toggleCreateBookmarkTab(false)
+  toggleViewFeedTab(true)
+}
 
-  let viewFeedContElem = document.getElementById('view-feed-cont')
-  viewFeedContElem.style.display = ""
-
-  document.getElementById('view-feed-tab').classList.add("selected-tab")
-  document.getElementById('create-bookmark-tab').classList.remove("selected-tab")
+function showViewBooksmarksTab() {
+  toggleCreateBookmarkTab(false)
+  toggleViewFeedTab(false)
+  toggleViewBookmarksTab(true)
 }
 
 let bookmarkTabElem = document.getElementById("create-bookmark-tab")
@@ -228,9 +263,53 @@ let feedTabElem = document.getElementById("view-feed-tab")
 feedTabElem.addEventListener("click", async() => {
   showViewFeedTab()
 })
+document.getElementById("view-bookmarks-tab").addEventListener("click", async() => {
+  showViewBooksmarksTab()
+})
 
 let loginPageBtn = document.getElementById("login-page-btn")
 loginPageBtn.addEventListener("click", async() => {
   chrome.tabs.create({url: 'views/login.html'}) 
 })
 
+let getUserUrls = async() => {
+  let response = null
+  try {
+    response = await axios.get(`${CONFIG.API_ENDPOINT}users/${USER_ID}`)
+    if (response && response.data && response.data.urls) {
+      return response.data.urls
+    }
+  } catch (e) {
+    console.log('error caught in getUserUrls(): ')
+    console.log(e)
+  }
+  console.log('response from request in getUserUrls():')
+  console.log(response)
+  return response
+}
+
+// TODO: to abstract to any table, should probably pass in the table id
+// need to create functions for getting the data from the url object
+let populateUserFeed = async() => {
+  let urls = await getUserUrls()
+  console.log("urls from response of getUserUrls():")
+  console.log(urls)
+  if (urls != null) {
+    let tableRows = "" 
+    for (const url of urls ) {
+     tableRows = tableRows + `
+      <tr>
+        <td>${url.custom_title || url.document_title}</td>
+        <td>${(url.rating == null ? 'None' : url.rating)}</td>
+        <td>${url.created_at}</td>
+      </tr>
+     ` 
+    }
+    let feedTableBodyElem = document.getElementById("bookmarks-table-body")  
+    feedTableBodyElem.innerHTML = tableRows
+  } else {
+    alert("Not able to retrieve urls from API!")
+  }
+}
+
+populateUserFeed()
