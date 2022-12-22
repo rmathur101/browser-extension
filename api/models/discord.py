a5e78815f852b590d8067c2e99c563092a7cf03f
Rohan Mathur <rmathur101@gmail.com>
2022-12-22 15:44:27 -0600

    do not require user_id to be sent when creating or authenticating with discord, we dont need the user id, well just use the authentication step to get the discord id, and use that to search our db and create a new user if need be

--- api/models/discord.py
+++ api/models/discord.py
@@ -3,5 +3,6 @@ from sqlmodel import SQLModel
 
 
 class DiscordAddUserData(SQLModel):
-    user_id: int
+    # RM: I'm commenting out the user_id as a parameter because the endpoint that used this validation will not require user id, instead it will check based on the discord code if the user associated with the discord id is already in the db, if it is, then it is just like a login step, if it is not, then we create the user
+    # user_id: int
     code: str
