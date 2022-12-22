a0510e2cb68a5ebc93ec0e2855dca522b661f3bd
Rohan Mathur <rmathur101@gmail.com>
2022-12-22 15:37:53 -0600

    add debugger listener to run python debugger on backend; add logging to 422 requests

--- api/main.py
+++ api/main.py
@@ -1,12 +1,11 @@
+# RM: This is to allow debugging in VSCode, I have to use it in my case, and Josca doesn't, I would guess because I'm not able to have multiple things listening on the same port because I'm running local development
 import dotenv
 config = dotenv.dotenv_values()
-DEBUG = config["DATA_DIR"]
-
-# RM: This is to allow debugging in VSCode, I have to use it in my case, and Josca doesn't, I would guess because I'm not able to have multiple things listening on the same port because I'm running local development
-if DEBUG == "True":
+DEBUG = config["DEBUG"]
+if DEBUG == "1":
     import ptvsd
     ptvsd.enable_attach(address=('127.0.0.1', 4000))
-    ptvsd.wait_for_attach()
+    # ptvsd.wait_for_attach()
 
 from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
@@ -14,6 +13,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from api.router import url_user
 from router import user, discord
+from logger import logger
 
 
 app = FastAPI()
@@ -25,6 +25,7 @@ app.include_router(discord.router)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    logger.error("RequestValidationError", error_type='RequestValidationError', status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=exc.errors(), body=exc.body)
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
@@ -33,5 +34,4 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
