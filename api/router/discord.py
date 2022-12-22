bfd699419ad5b262b3252f3af69a396fc19724b3
Rohan Mathur <rmathur101@gmail.com>
2022-12-22 15:53:32 -0600

    modify discord endpoint so that we dont require the user id and that we check if user exists based on the discord id and if they dont then we create a user like that

--- api/router/discord.py
+++ api/router/discord.py
@@ -1,10 +1,11 @@
 from typing import List
-from api import models, crud
+from api import models, crud 
 from fastapi import Depends, HTTPException, APIRouter
 import dotenv
 from api.discord_utils import Oauth
 import json
 from pathlib import Path
+from logger import logger
 
 config = dotenv.dotenv_values()
 DATA_DIR = Path(config["DATA_DIR"])
@@ -15,6 +16,7 @@ discord_oauth = Oauth(
     config["REDIRECT_URL"],
     scope="identify email guilds",
 )
+
 
 
 router = APIRouter()
@@ -28,36 +30,52 @@ async def go_to_discord_authentication_page():
 
 @router.post("/discord")
 async def update_user_discord_data(discord_user: models.DiscordAddUserData):
-    tokens = discord_oauth.get_access_token(code=discord_user.code)
-    discord_data = discord_oauth.get_user(access_token=tokens["access_token"])
-    guilds = discord_oauth.get_guilds(access_token=tokens["access_token"])
+    try:
 
-    # Check if user is in 1729 guild
-    if not any([guild["id"] == GUILD_ID_1729 for guild in guilds]):
-        raise HTTPException(status_code=404, detail="User not in 1729")
+        # Get discord tokens using code from authentication and check if access token is available 
+        tokens = discord_oauth.get_access_token(code=discord_user.code)
+        if (not ("access_token" in tokens)):
+            raise HTTPException(status_code=500, detail="No access token found with the discord code that was sent after authentication.")
 
-    user = models.User(
-        id=discord_user.user_id,
-        discord_id=discord_data["id"],
-        discord_username=discord_data["username"],
-        discord_avatar=discord_data["avatar"],
-    )
-    user = crud.user.update(user)
+        # Use tokens to get discord data including guilds
+        discord_data = discord_oauth.get_user(access_token=tokens["access_token"])
+        guilds = discord_oauth.get_guilds(access_token=tokens["access_token"])
 
-    # Check if user has shared any urls in discord. If so update user_id
-    # for these urls and delete temp user created for discord links
-    user_discord_only = crud.user.where(
-        dict(discord_id=discord_user.user_id, email="discord_only")
-    )
-    if user_discord_only:
-        user_discord_only = user_discord_only[0]
-        crud.url_user._update_user_for_discord_urls(
-            user_id=user.id, discord_user_id=user_discord_only.id
-        )
-        crud.user.delete(id=user_discord_only.id)
+        # Check if user is in 1729 guild
+        if not any([guild["id"] == GUILD_ID_1729 for guild in guilds]):
+            raise HTTPException(status_code=500, detail="This user does not have the 1729 guild in their guilds list.")
 
-    return {"detail": "Discord data updated"}
+        # RM: check if the user with this discord id already exists
+        user = crud.user.get_by_discord_id(discord_data["id"]) 
+        if (not user):
+            user = models.User(
+                discord_id=discord_data["id"],
+                discord_username=discord_data["username"],
+                discord_avatar=discord_data["avatar"],
+            )
+            user = crud.user.create(user)
 
+            logger.info("New user created.", user_id=user.id, discord_username=user.discord_username)
+            return {"status_code": 200, "detail": "User created"}
+        else:
+            logger.info("This user already has account.", user_id=user.id, discord_username=user.discord_username)
+            return {"status_code": 200, "detail": "User already has account."}
+
+        # RM NOTE: I'm commenting this out because I don't think we need it. If we do, we can uncomment it. Basically I can't think of why assigning the shares to a user after they logged in would be important, this should be fine for now.
+        # Check if user has shared any urls in discord. If so update user_id
+        # for these urls and delete temp user created for discord links
+        # user_discord_only = crud.user.where(
+        #     dict(discord_id=discord_user.user_id, email="discord_only")
+        # )
+        # if user_discord_only:
+        #     user_discord_only = user_discord_only[0]
+        #     crud.url_user._update_user_for_discord_urls(
+        #         user_id=user.id, discord_user_id=user_discord_only.id
+        #     )
+        #     crud.user.delete(id=user_discord_only.id)
+    except Exception as e:
+        logger.error("Error in /discord endpoint", error=e, discord_user=discord_user)
+        return {"status_code": 500}
 
 @router.get("/discord/{user_id}")
 async def get_user_channels_and_threads(user_id):
