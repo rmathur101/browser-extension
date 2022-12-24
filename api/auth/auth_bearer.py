abfd5695850983aa2b760ef0d35edcca65f5afa7
Rohan Mathur <rmathur101@gmail.com>
2022-12-23 21:17:53 -0600

    use try catch and log exceptions with JWT class

--- api/auth/auth_bearer.py
+++ api/auth/auth_bearer.py
@@ -3,6 +3,7 @@
 from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
+from logger import logger
 from .auth_handler import decodeJWT
 
 class JWTBearer(HTTPBearer):
@@ -10,19 +11,22 @@ class JWTBearer(HTTPBearer):
         super(JWTBearer, self).__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request):
-        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
-        if credentials:
-            if not credentials.scheme == "Bearer":
-                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
-            if not self.verify_jwt(credentials.credentials):
-                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
-            return credentials.credentials
-        else:
-            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+        try:
+          credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+          if credentials:
+              if not credentials.scheme == "Bearer":
+                  raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+              if not self.verify_jwt(credentials.credentials):
+                  raise HTTPException(status_code=403, detail="Invalid token or expired token.")
+              return credentials.credentials
+          else:
+              raise HTTPException(status_code=403, detail="Invalid authorization code.")
+        except Exception as e:
+          logger.error("JWT Authentication failed.", error=e)
+          raise HTTPException(status_code=403, detail="JWT Authentication failed.") 
 
     def verify_jwt(self, jwtoken: str) -> bool:
         isTokenValid: bool = False
-
         try:
             payload = decodeJWT(jwtoken)
         except:
